5822fb40aa5432e00259e972fd72c47a3f67e175
Macintosh <tao9435@126.com>
2016-04-25 13:33:24 +0800

    init

---
+++ KMHouse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KMHouse
+//
+//  Created by thinker on 16/4/25.
+//  Copyright © 2016年 Trefoil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pods/Target Support Files/Pods-KMHouse/Pods-KMHouse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KMHouse : NSObject
+@end
+@implementation PodsDummy_Pods_KMHouse
+@end
